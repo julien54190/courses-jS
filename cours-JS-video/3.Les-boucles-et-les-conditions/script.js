64addcfc1effc8669936baf13b9f6c872da7795d
Julien Lucadello <lucadello.julien@hotmail.fr>
2023-06-06 11:19:11 +0200

    continuation loop's lesson

--- cours-JS-video/3.Les-boucles-et-les-conditions/script.js
+++ cours-JS-video/3.Les-boucles-et-les-conditions/script.js
@@ -41,4 +41,62 @@ const backgroundColor = isDarkModeOn ? "black" : "white"
 
 console.log('backgroungcolor', backgroundColor)
 
-//const ternerary = 
+
+// Boucle for
+// Pout toute mes .....(valeur)
+// Pour chacun de .... (enfants)
+// Pour tous les nombre paires / impaires
+// Pour les 10 premiers utilisateur ...
+
+// Array = tableau 
+const colorsString = ['blue', 'red', 'green']
+const colorsInt = [1, 2, 3]
+
+console.log("tableau", colorsString)
+console.log("bleu : ", colorsString[0])
+
+// programme : 
+// Calcule automatiquement une rédiction de 50%
+// sur les prix de mon magasin
+
+const prices = [10, 13, 14, 15]
+const pricesPromo = []
+
+for (let index = 0; index < prices.length; index++) {
+    const initialPrice = prices[index]
+    const priceWithPromo = initialPrice / 2
+    pricesPromo.push(priceWithPromo) // pour placé les valeur dans le tableau 
+    
+}
+
+console.log("Mes nouveau prix sont : ", pricesPromo)
+
+
+// tableau d'objet
+
+const shop =  [
+    {
+        name: "sac A",
+        price: 10
+    },
+    {
+        name: "sac B",
+        price: 13
+    },
+    {
+        name: "sac C",
+        price: 14
+    },
+    {
+        name: "sac D",
+        price: 15
+    }
+]
+
+for (let index = 0; index < shop.length; index++) {
+    const priceWithPromo = shop[index].price / 2
+    shop[index].promo = priceWithPromo
+}
+
+//console.log("shop", shop)
+console.table(shop)
